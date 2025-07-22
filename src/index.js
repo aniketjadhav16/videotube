660c2c10e2314ea96afe00b35614e0b427c4467b
Aniket Jadhav <jadhavaniket897@gmail.com>
2025-07-23 01:09:26 +0530

    updated utils folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: "./env"
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.on("Error", (error) => {
+      console.log(`Error: ${error}`);
+      throw error;
+    });
+  })
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+
+  .catch((err) => {
+    console.log("MongoDB connection failed!", err);
+  });
